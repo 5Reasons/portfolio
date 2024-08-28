769ba658468a347bde8b9c871f376d06fd493708
5Reasons <764432291@qq.com>
2024-08-29 03:15:38 +0800

    fix: loader in mid

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,14 +4,23 @@ import { Html, useProgress } from "@react-three/drei";
 const Loader = () => {
   const { progress } = useProgress();
   return (
-    <Html>
+    <Html
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%", // edit
+      }}
+    >
       <span className="canvas-load"></span>
       <p
         style={{
           fontSize: 14,
           color: "#f1f1f1",
           fontWeight: 800,
-          marginTop: 40,
+          marginTop: 20,
+          marginLeft: 10, // edit
         }}
       >
         {progress.toFixed(2)}%
